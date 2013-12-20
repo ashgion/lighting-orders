6213ecdee0dbc1fc1d26f2a1b046c45849f83f1a
Ashley Gionfriddo <ashley.marie812@gmail.com>
2013-12-20 11:36:59 -0700

    test commit

---
+++ db/migrate/20131120184617_employee.rb
@@ -0,0 +1,6 @@
+class Employee < ActiveRecord::Migration
+  def change
+  create_table :employees do |t|
+  
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,33 @@
+Employee.create(id: 1, username: "Ashley", password: "123")
+Employee.create(id: 2, username: "Hannah", password: "456")
+
+
+
+
+
+
+Product.create! \
+item: "Noah",
+description: "15",
+cost: "569"
+
+Product.create! \
+item: "Ashley",
+description: "7",
+cost: "569"
+
+Product.create! \
+item: "Mike",
+decription: "9",
+cost: "569"
+
+Product.create! \
+item: "Josh",
+decription: "15",
+cost: "569"
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
